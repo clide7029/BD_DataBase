0aadb85e9ba7c7d4c6059e51619fa10a8eb9ef09
Andrewab <andrewalcideblanchette@gmail.com>
2021-10-22 12:42:18 -0700

    Update createPassenger.php

--- public_html/createPassenger.php
+++ public_html/createPassenger.php
@@ -43,7 +43,7 @@ $ssn = $_POST['ssn'];
 echo $errorstring;
 if($errorstring != ""){
     //header('Location: ./inputForm.html');
-    echo "<script>alert('INVALID FIELDS'.concat('<?php echo $errorstring'));
+    echo "<script>alert('INVALID FIELDS'.concat('echo $errorstring'));
         window.location.href = './inputForm.html';
     </script>"; //make this send to skyler
     //echo '<script type="text/JavaScript"> prompt("GeeksForGeeks");</script>';
