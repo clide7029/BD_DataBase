d3e4153401f18fd330bf00d4b32051e85fc97d19
BrianMcMurr <61162591+BrianMcMurr@users.noreply.github.com>
2021-10-22 13:01:25 -0700

    Update updatePassenger.php

--- public_html/updatePassenger.php
+++ public_html/updatePassenger.php
@@ -4,38 +4,80 @@ $attribute = $_POST['attribute'];
 $first ="";
 $middle = "";
 $last = "";
+$errorstring = "";
+if(strlen($ssn)!=11){
+    $errorstring .= "SSN_Wrong_Length ";
+}
 
+$tempSSN = explode("-",$ssn);
+$tempSSN = implode("", $tempSSN);
+if(!is_numeric($tempSSN)){
+    $errorstring .= "SSN_Not_Numeric ";
+}
 if($attribute == "first"){
     $first =$_POST['value'];
+    if($first == ""){
+        $errorstring .= "First_Empty ";
+    }
+    if($first == NULL){
+        $errorstring .= "First_Null ";
+    }
+    if(!ctype_alpha($first)){
+        $errorstring .= "First_Non-Alphabetical ";
+    }
 }
 if($attribute == "middle"){
     $middle =$_POST['value'];
 }
 if($attribute == "last"){
     $last =$_POST['value'];
+    if($last == ""){
+        $errorstring .= "Last_Empty ";
+    }
+    if($last == NULL){
+        $errorstring .= "Last_Null ";
+    }
+    if(!ctype_alpha($last)){
+        $errorstring .= "Last_Non-Alphabetical ";
+    }
 }
+if($errorstring != ""){
+    //header('Location: ./inputForm.html');
+    echo "<script>alert('INVALID FIELDS'.concat(' $errorstring'));
+        window.location.href = './inputForm.html';
+    </script>"; //make this send to skyler
+    //echo '<script type="text/JavaScript"> prompt("GeeksForGeeks");</script>';
+    //exit("");
+    exit("");
 
+}
 $db_file = './myDB/airport.db';
-try {
+ try {
 
-    //open connection to the airport database file
-    $db = new PDO('sqlite:' . $db_file);      // <------ Line 13
+//     //open connection to the airport database file
+     $db = new PDO('sqlite:' . $db_file);      // <------ Line 13
 
-    //set errormode to use exceptions
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     //set errormode to use exceptions
+     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     //return all passengers, and store the result set
-    $stmt = $db -> prepare("update passengers set (first) values (:first, :middle, :last, :ssn);");  // <----- Line 19
-    $stmt->bindParam(':first', $first);
-    $stmt->bindParam(':middle', $middle);
-    $stmt->bindParam(':last', $last);
-    $stmt->bindParam(':ssn', $ssn);
-    execute($stmt);
-    $_SESSION['successinsert'] = true;
-    header('Location: ./showPassenger.php');
-}
-catch(PDOException $e) {
-    die('Exception : '.$e->getMessage());
-}
-
+    if($attribute == "first"){
+     $stmt = $db -> prepare("update passengers set f_name = (?);");  // <----- Line 19
+     $stmt-> execute([$_POST['first']);
+    }
+    if($attribute == "middle"){
+        $stmt = $db -> prepare("update passengers set m_name = (?);");  // <----- Line 19
+        $stmt-> execute([$_POST['middle']);
+       }
+    if($attribute == "last"){
+        $stmt = $db -> prepare("update passengers set l_name = (?);");  // <----- Line 19
+        $stmt-> execute([$_POST['last']);
+    }
+    echo "success";
+      $_SESSION['successinsert'] = true;
+      header('Location: ./showPassengers.php');
+ }
+ catch(PDOException $e) {
+     die('Exception : '.$e->getMessage());
+ }
 ?>
