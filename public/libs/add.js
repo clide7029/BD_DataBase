f259adf290cee025c17b27fbdda8d90e6858c5fc
BrianMcMurr <61162591+BrianMcMurr@users.noreply.github.com>
2021-12-02 18:42:36 -0800

    creates tables and inserts

---
+++ public/libs/add.js
@@ -0,0 +1,7 @@
+var sqlite3 = require('sqlite3').verbose();
+var insertTable= '';
+var insertVariables='';
+var db = new sqlite3.Database('.../data/database.db');
+var stmt = db.prepare("INSERT INTO (?) VALUES (?)");
+stmt.run(insertTable,insertVariables);
+stmt.finalize();
