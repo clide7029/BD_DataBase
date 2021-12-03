32ca9609d69a093db8a3b4884534f796b86b5868
clide7029 <skylerwb7029@gmail.com>
2021-12-02 18:04:23 -0800

    restruct

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 8080 //http = 80, ssh = 22
 const express = require("express");
 const app = express();
 
-app.use(express.static("/public"));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.listen(port, () => {
