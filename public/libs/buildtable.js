2f045f45fbde8e800c795b192015e528ea6cec20
BrianMcMurr <61162591+BrianMcMurr@users.noreply.github.com>
2021-12-07 21:26:46 -0800

    Made new database funcitons
    also updated database buildtable to account for new pricing. No code breaks new code having some trouble

--- public/libs/buildtable.js
+++ public/libs/buildtable.js
@@ -18,7 +18,7 @@ module.exports = {
                 db.run('create table Owned(username TEXT,ticker TEXT,numshares int,PRIMARY KEY (username,ticker),FOREIGN key (username) REFERENCES User,FOREIGN key (ticker) REFERENCES Ticker)');
                 db.run('create table Portfolio(username TEXT,ticker TEXT,numshares int,mostrecentprice REAL,PRIMARY KEY (username,ticker),FOREIGN key (username) REFERENCES User,FOREIGN key (ticker) REFERENCES Ticker)');
                 db.run('create table Sale (username text,ticker text,day text,hour text,boughtsold int,PRIMARY key (ticker,day,hour,username),FOREIGN key (username) REFERENCES User,FOREIGN key (ticker) REFERENCES Ticker)');
-                db.run('create table Pricing(ticker TEXT PRIMARY KEY,day text,openprice double,closeprice double,FOREIGN key (ticker) REFERENCES Ticker)');
+                db.run('create table Pricing(ticker TEXT PRIMARY KEY,day text,openPrice double,closePrice double,highPrice double,lowPrice double,FOREIGN key (ticker) REFERENCES Ticker)');
                 db.run('create table Return(ticker text PRIMARY key,day text,return text,FOREIGN key (ticker) REFERENCES Ticker)');
                 });
         
